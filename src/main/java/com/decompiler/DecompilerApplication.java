ffddc9eac74a151a09d5cc239ee86995c6d3a497
AjinkyaAde20 <166989360+AjinkyaAde20@users.noreply.github.com>
2026-03-28 17:04:41 +0530

    Add Spring Boot main application class

---
+++ src/main/java/com/decompiler/DecompilerApplication.java
@@ -0,0 +1,12 @@
+package com.decompiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecompilerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecompilerApplication.class, args);
+    }
+}
